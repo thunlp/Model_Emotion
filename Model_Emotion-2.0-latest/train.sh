d8874a0f096b20202b72bf5d2e68ab1cdcfe726f
Yusheng Su <yushengsu.thu@gmail.com>
2023-07-02 16:23:20 +0800

    update

---
+++ Model_Emotion-2.0-latest/train.sh
@@ -0,0 +1,19 @@
+#sentiment can be 'realization' 'surprise' 'admiration' 'gratitude' 'optimism' 'approval' 'pride' 'excitement' 'joy' 'love' 'amusement' 'caring' 'relief' 'curiosity' 'desire' 'disgust' 'disapproval' 'anger' 'annoyance' 'confusion' 'fear' 'disappointment' 'grief' 'sadness' 'nervousness' 'embarrassment' 'remorse'
+
+# seed can be {1,3,5,7,9,11,13,15,17,19,42,100}
+
+
+for sentiment in 'surprise'
+do
+  for seed in {1}
+  do
+    echo "sentiment=$sentiment,random_seed=$seed"
+    CUDA_VISIBLE_DEVICES=0 \
+    python train.py \
+    --output_dir outputs \
+    --backbone roberta-base \
+    --prompt_len 100 \
+    --sentiment=$sentiment \
+    --seed $seed
+  done
+done
