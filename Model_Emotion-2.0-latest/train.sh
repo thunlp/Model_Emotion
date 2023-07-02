dd88e78d94ac84e292d94367f858d21aab9f6aed
Yusheng Su <ky200120000@gmail.com>
2023-07-02 16:39:48 +0800

    update

--- Model_Emotion-2.0-latest/train.sh
+++ Model_Emotion-2.0-latest/train.sh
@@ -1,18 +1,21 @@
-#sentiment can be 'realization' 'surprise' 'admiration' 'gratitude' 'optimism' 'approval' 'pride' 'excitement' 'joy' 'love' 'amusement' 'caring' 'relief' 'curiosity' 'desire' 'disgust' 'disapproval' 'anger' 'annoyance' 'confusion' 'fear' 'disappointment' 'grief' 'sadness' 'nervousness' 'embarrassment' 'remorse'
+BACKBONE=roberta-base
+PROMPTLEN=100
 
-# seed can be {1,3,5,7,9,11,13,15,17,19,42,100}
 
-
-for sentiment in 'surprise'
+for sentiment in 'realization' 'surprise' 'admiration' 'gratitude' 'optimism' 'approval' \
+                 'pride' 'excitement' 'joy' 'love' 'amusement' 'caring' \
+                 'relief' 'curiosity' 'desire' 'disgust' 'disapproval' 'anger' \
+                 'annoyance' 'confusion' 'fear' 'disappointment' 'grief' 'sadness' \
+                 'nervousness' 'embarrassment' 'remorse'
 do
-  for seed in 1
+  for seed in {1,3,5,7,9,11,13,15,17,19,42,100}
   do
     echo "sentiment=$sentiment,random_seed=$seed"
     CUDA_VISIBLE_DEVICES=0 \
     python train.py \
     --output_dir outputs \
-    --backbone roberta-base \
-    --prompt_len 100 \
+    --backbone $BACKBONE \
+    --prompt_len $PROMPTLEN \
     --sentiment=$sentiment \
     --seed $seed
   done
